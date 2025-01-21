9ccc7fa531c6f386ba6e2cacb4c946d568fe45f6
sarveshcoditron <sarvesh.ransubhe@coditron.com>
2025-01-21 18:24:46 +0530

    hydrated bloc added

--- lib/config_main.dart
+++ lib/config_main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_app/src/di/get_it.dart';
 import 'package:movie_app/src/presentation/cubits/bloc_observer.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> configMain() async {
   configureDependencies();
   Bloc.observer = MyBlocObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: HydratedStorageDirectory(
+      (await getTemporaryDirectory()).path,
+    ),
+  );
   WidgetsFlutterBinding.ensureInitialized();
 
   await Future.wait([
--- lib/src/presentation/cubits/home_page/home_page_cubit.dart
+++ lib/src/presentation/cubits/home_page/home_page_cubit.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
-
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:injectable/injectable.dart';
 import 'package:movie_app/src/data/model/movie.dart';
@@ -12,7 +11,7 @@ import 'package:movie_app/src/domain/usecases/get_movies_list.dart';
 part 'home_page_state.dart';
 
 @injectable
-class HomeCubit extends Cubit<HomeState> {
+class HomeCubit extends Cubit<HomeState> with HydratedMixin {
   final GetMoviesList _getMoviesList;
 
   final _pageSize = 20;
@@ -20,7 +19,9 @@ class HomeCubit extends Cubit<HomeState> {
   final PagingController<int, Movie> _pagingController =
       PagingController(firstPageKey: 1);
 
-  HomeCubit(this._getMoviesList) : super(HomeInitial());
+  HomeCubit(this._getMoviesList) : super(HomeInitial()) {
+    hydrate();
+  }
 
   void intialize() {
     _pagingController.addPageRequestListener((pageKey) {
@@ -49,5 +50,17 @@ class HomeCubit extends Cubit<HomeState> {
         // emit(HomeLoaded(r));
       },
     );
+  }
+
+  @override
+  HomeState? fromJson(Map<String, dynamic> json) {
+    // TODO: implement fromJson
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<String, dynamic>? toJson(HomeState state) {
+    // TODO: implement toJson
+    throw UnimplementedError();
   }
 }
